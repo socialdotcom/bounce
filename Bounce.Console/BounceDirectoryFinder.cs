63932b6c391e82a9f5a49398a6c7ff3bc1427784
Olivier Kouame <olivier.kouame@gmail.com>
2014-09-05 09:31:52 +0100

    Changed Bounce.exe to look for Bounce tasks in a configuration location set in the app.config

--- Bounce.Console/BounceDirectoryFinder.cs
+++ Bounce.Console/BounceDirectoryFinder.cs
@@ -1,24 +1,33 @@
+using System.Configuration;
 using System.IO;
 
-namespace Bounce.Console {
-    class BounceDirectoryFinder : IBounceDirectoryFinder {
-        public string FindBounceDirectory() {
+namespace Bounce.Console
+{
+    class BounceDirectoryFinder : IBounceDirectoryFinder
+    {
+        private readonly string directoryName;
+
+        public BounceDirectoryFinder()
+        {
+            directoryName = ConfigurationManager.AppSettings["Tasks.Directory.Name"];
+            if (directoryName == null) throw new ConfigurationException("The AppSetting key Tasks.Directory.Name was not found.");
+        }
+
+        public string FindBounceDirectory()
+        {
             return FindBounceDirectoryFrom(Directory.GetCurrentDirectory());
         }
 
-        private string FindBounceDirectoryFrom(string dir) {
-            if (string.IsNullOrEmpty(dir)) {
+        private string FindBounceDirectoryFrom(string dir)
+        {
+            if (string.IsNullOrEmpty(dir))
+            {
                 return null;
             }
 
-            var bounceDir = Path.Combine(dir, "Bounce");
+            var bounceDir = Path.Combine(dir, directoryName);
 
-            if (Directory.Exists(bounceDir))
-            {
-                return bounceDir;
-            } else {
-                return FindBounceDirectoryFrom(Path.GetDirectoryName(dir));
-            }
+            return Directory.Exists(bounceDir) ? bounceDir : FindBounceDirectoryFrom(Path.GetDirectoryName(dir));
         }
     }
 }
